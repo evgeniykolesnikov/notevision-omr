28fd35ec38935724b220e492ebedc3146f768959
evgeniykolesnikov <32667874+evgeniykolesnikov@users.noreply.github.com>
2026-07-09 17:20:39 +0300

    chore: add experimental package markers

---
+++ src/notevision/experimental/__init__.py
@@ -0,0 +1,1 @@
+"""Experimental features that are not part of the main VKR pipeline."""
---
+++ src/notevision/experimental/neural_omr/__init__.py
@@ -0,0 +1,6 @@
+"""Toy neural OMR proof-of-concept.
+
+This package is experimental. It does not replace Audiveris and does not
+produce full MusicXML.
+"""
+
